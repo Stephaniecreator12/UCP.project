e1768f8738672887a9d21e7fe49512a23a916888
Stephanie <stephaniehanitriniala4@gmail.com>
2026-05-30 04:37:11 +0300

    seed demo users during render build

--- build.sh
+++ build.sh
@@ -4,3 +4,7 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+if [ "${SEED_DEMO_USERS:-False}" = "True" ]; then
+  python manage.py seed_role_users --password "${DEMO_USERS_PASSWORD:-test12345}" --reset-password
+fi

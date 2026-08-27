72628563f05130b4c10417b46465c5e0f332a57d
playsteboy <mikaiahranaivoson@gmail.com>
2026-08-27 08:41:01 +0300

    chore: migrate frontend to pnpm

--- backend_PPM/config/settings.py
+++ backend_PPM/config/settings.py
@@ -135,7 +135,7 @@ DATABASES = {
     'default': {
         'ENGINE': os.getenv("DB_ENGINE", 'django.db.backends.postgresql'),
         'NAME': os.getenv("DB_NAME", 'passation_db'),
-        'USER': os.getenv("DB_USER", 'postgres'),
+        'USER': os.getenv("DB_USER", 'passation_manager'),
         'PASSWORD': os.getenv("DB_PASSWORD", 'passation'),
         'HOST': os.getenv("DB_HOST", 'localhost'),
         'PORT': os.getenv("DB_PORT", '55432'),
